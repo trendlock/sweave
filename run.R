95671ce9fb6cf08dbcac686b159508f568862709
rosseji <rosseireland@gmail.com>
2018-07-31 12:46:53 +1000

    bump

--- run.R
+++ run.R
@@ -10,5 +10,6 @@ complile_pdf <- function(base_nm) {
   knitr::knit(paste0(base_nm, ".Rnw"), quiet = T)
   tools::texi2pdf(paste0(base_nm, ".tex"), quiet = T)
 }
+base_nm <- "example"
 
 complile_pdf("example")
